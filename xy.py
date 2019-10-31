d1477103d77e4791585886bbbecf695327116bc4
websiteaap <53229705+websiteaap@users.noreply.github.com>
2019-11-01 00:03:24 +0530

    Add files via upload

---
+++ xy.py
@@ -0,0 +1,6 @@
+print("\t\tFind the x square of y")
+print("1 - x id=s the base number \n 2 - y is the power of x")
+x = float(input("Enter the base number : "))
+y = float(input("Enter the power of x value : "))
+z = x**y
+print("The",x,"square of",y ,"is =",z)
